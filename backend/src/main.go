0c9b1a22f927d9c5e14fb996db29353aa9c64dde
Shaurya A Wahal <shauryawahal@gmail.com>
2022-02-04 17:47:18 -0500

    init

--- backend/src/main.go
+++ backend/src/main.go
@@ -17,8 +17,8 @@ type Car struct {
 
 func main() {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	if err !=  nil {
-		panic("failed to connect database")
+	if err != nil {
+		panic("failed to connect to database")
 	}
 
 	db.AutoMigrate(&Car{})
